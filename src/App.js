6bf6f03a04e1256c9463559da34492152dfe6760
Hanul Kim <109802425+gotitlikethat@users.noreply.github.com>
2022-11-06 15:53:58 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from "react";
+import axios from "axios";
+import Movie from './Movie';
+import Header from './Header';
+import './App.css';
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: {movies},
+      },
+    } = await axios.get('http://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({movies, isLoading: false});
+  }
+  componentDidMount() {
+    // 영화 데이터 로딩!
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div class="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className = "movies">
+            <div className = "header">
+            <Header />
+          </div>
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.large_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    )
+  }
+}
+
+export default App;
